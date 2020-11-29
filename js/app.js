b0432b15469529aac80024ff3aedb49ba50fe129
Maksym <maksymRozhko7@gmail.com>
2020-11-30 01:19:27 +0200

    feat: added burger and slider;
    - fixed styles;
    - fixed cross-browser compatibility
    - added a burger menu for tablet and mobile extensions.
    - added a slider to the main screen.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,97 @@
 // smooth scrolling by anchors
-// const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
-// const animationTime = 300;
-// const framesCount = 20;
-
-// anchors.forEach((item) => {
-//     item.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-
-//         let scroller = setInterval(() => {
-//             let scrollBy = coordY / framesCount;
-
-//             if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-//                 window.scrollBy(0, scrollBy);
-//             } else {
-//                 window.scrollTo(0, coordY);
-//                 clearInterval(scroller);
-//             }
-//         }, animationTime / framesCount);
-//     });
-// });
-
-const anchors = document.querySelectorAll('a.navigation-link')
+const headerBlock = document.querySelector('.header');
+const sectionElem = document.getElementsByTagName('section');
+const overflowFoter = document.querySelector('.footer');
+const navMenu = document.querySelector('.navigation');
+const anchors = document.querySelectorAll('a.navigation-link');
+const burgerMenu = document.querySelector('#burgerToogle');
 
 for (let anchor of anchors) {
+  const blockID = anchor.getAttribute('href');
   anchor.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href')
+    e.preventDefault();
     
     document.querySelector(blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     });
+    if (anchor.classList.value === 'nav-link--active' || anchor.classList.value !== 'nav-link--active') {
+      for (let anchor of anchors) {
+        anchor.classList.remove('nav-link--active');
+      }
+      anchor.classList.add('nav-link--active');
+    } else {
+      false;
+    }
   });
 };
+
+//burger menu
+burgerMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  navMenu.classList.toggle('nav-menu--active');
+  headerBlock.classList.toggle('overflow');
+  overflowFoter.classList.toggle('overflow'); 
+
+  for (let classListName of sectionElem ) {
+    const sectionElemValue = classListName.classList.value;
+    sectionElemValue !== 'overflow' ? classListName.classList.toggle('overflow') : false;
+  }
+});
+
+//header slider 
+const sliderImg = document.querySelector('#sliderImg');
+const btnHeaderPrevSlide = document.querySelector('.left');
+const btnHeaderNextSlide = document.querySelector('.right');
+const radioWrapp = document.querySelector('.radio-wrapper');
+const radioBtns = document.querySelectorAll('.radio-off');
+
+let slideImg = 0;
+
+const imgItems = [
+  './css/img/features/f6.png',
+  './css/img/Logo/bg-header.png',
+  './css/img/posts/photo-post1.png',
+  './css/img/posts/photo-post2.png',
+  './css/img/video/img-video.png'
+];
+
+const nextSlide = () => {
+  sliderImg.style.display = 'block';
+    radioBtns[1].classList.add('button-on');
+  if (slideImg < (imgItems.length - 1)) {
+    slideImg++;
+  } else {
+    slideImg = 0;
+  }
+  sliderImg.src = `${imgItems[slideImg]}`;
+
+  // const currDot = [slideImg];
+  // console.log(currDot);
+}
+
+const prevSlide = () => {
+  if (slideImg > 0) {
+    slideImg--;
+  } else {
+    slideImg = imgItems.length - 1;
+  }
+  sliderImg.src = `${imgItems[slideImg]}`;  
+}
+
+radioBtns.forEach((radioSelected) => {
+  radioSelected.addEventListener('click',function() {
+     if (radioSelected.classList.value === 'button-on' || radioSelected.classList.value !== 'button-on') {
+      for (let radioSelected of radioBtns) {
+        radioSelected.classList.remove('button-on');
+      }
+      radioSelected.classList.add('button-on');
+    } else {
+      false
+    }
+  });
+});
+
+nextSlide();
+btnHeaderNextSlide.addEventListener('click', nextSlide);
+btnHeaderPrevSlide.addEventListener('click', prevSlide);
